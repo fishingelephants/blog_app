2aee8a0cfb0bafc79d91b3645c556adf57aa866d
Leif Latiff <eightminds@gmail.com>
2017-01-24 21:13:47 +0000

    Link articles to users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,62 +1,64 @@
 class ArticlesController < ApplicationController
-#before_action :authenticate_user!, except: [:index, :show]
-before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
   end
-  
+
   def show
   end
-  
+
   def new
     @article = Article.new
   end
-  
+
   def create
-    @article = Article.new(article_params) 
+    @article = Article.new(article_params)
+    @article.user = current_user
+
     if @article.save
-      flash[:success] = "Article has been created"
-      redirect_to articles_path 
+      flash[:notice] = "Article has been created"
+      redirect_to articles_path
     else
-      flash.now[:danger] = "Article has not been created"
+      flash.now[:alert] = "Article has not been created"
       render :new
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @article.update(article_params)
       flash[:success] = "Article has been updated"
-      redirect_to @article 
+      redirect_to @article
     else
       flash.now[:danger] = "Article has not been updated"
       render :edit
     end
   end
-  
+
   def destroy
     if @article.destroy
       flash[:success] = "Article has been deleted."
-      redirect_to articles_path 
+      redirect_to articles_path
     end
   end
-  
-  protected
-    def resource_not_found
-      message = "The article you are looking for could not be found" 
-      flash[:alert] = message
-      redirect_to root_path
-    end
-    
-  private
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    def article_params
-      params.require(:article).permit(:title, :body) 
-    end
+  protected
+
+  def resource_not_found
+    flash[:alert] = "The article you are looking for could not be found"
+    redirect_to root_path
+  end
+
+private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
